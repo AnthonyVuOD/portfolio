77e58925b3facd2401ad44d143a89b03856953fd
Tony Vu <AnthonyVuOD@gmail.com>
2016-08-28 16:13:44 -0500

    08282016

--- js/portfolio.js
+++ js/portfolio.js
@@ -15,9 +15,9 @@ var app = angular
         templateUrl:'templates/portfolio.html',
         controller:'portfolioCTRL'
       })
-      .when("/workflow",{
-        templateUrl:'templates/workflow.html',
-        controller:'workflowCTRL'
+      .when("/cv",{
+        templateUrl:'templates/cv.html',
+        controller:'cvCTRL'
       })
       .when("/contact",{
         templateUrl:'templates/contact.html',
@@ -27,44 +27,48 @@ var app = angular
         redirectTo:"/home"
       })
   })
-  .controller('indexCTRL', function($rootScope, $scope, $location){
+  .controller('indexCTRL', function($rootScope, $scope, $location, $timeout){
     $rootScope.$on('$routeChangeSuccess', function(){
       $scope.landingPage = $location.path() === '/home';
     });
+    $scope.fade=false;
+    $timeout(function(){
+      $scope.fade=true;
+    },200);
   })
   .controller('homeCTRL', function($scope, $timeout){
     $scope.fade=false;
-      $timeout(function(){
-        $scope.fade=true;
-      },550);
+    $timeout(function(){
+      $scope.fade=true;
+    },550);
   })
   .controller('aboutCTRL', function($scope, $timeout){
     $scope.fade=false;
-      $timeout(function(){
-        $scope.fade=true;
-      },400);
+    $timeout(function(){
+      $scope.fade=true;
+    },400);
   })
   .controller('portfolioCTRL', function($scope, $timeout){
     $scope.items = [
-      {project: "project 1", color: "purple", delay: ".1s"},
-      {project: "project 2", color: "green", delay: ".2s"},
-      {project: "project 3", color: "red", delay: ".3s"},
-      {project: "project 4", color: "blue", delay: ".4s"}
+      {project: "Greenbelt Climbing Routes", color: "purple", delay: ".1s"},
+      {project: "Travis Perkins Photography", color: "green", delay: ".2s"},
+      {project: "How Much House?", color: "red", delay: ".3s"},
+      {project: "Vu Realty", color: "blue", delay: ".4s"}
     ];
     $scope.fade=false;
-      $timeout(function(){
-        $scope.fade=true;
-      },400);
+    $timeout(function(){
+      $scope.fade=true;
+    },400);
   })
-  .controller('workflowCTRL', function($scope, $timeout){
+  .controller('cvCTRL', function($scope, $timeout){
     $scope.fade=false;
-      $timeout(function(){
-        $scope.fade=true;
-      },400);
+    $timeout(function(){
+      $scope.fade=true;
+    },400);
   })
   .controller('contactCTRL',function($scope, $timeout){
     $scope.fade=false;
-      $timeout(function(){
-        $scope.fade=true;
-      },400);
+    $timeout(function(){
+      $scope.fade=true;
+    },400);
   });
